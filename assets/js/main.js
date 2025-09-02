0ff94341ff710defbd56fdefb63835da8ace9401
Harshpreet Singh <harshpreet170607@gmail.com>
2025-09-02 19:37:59 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Simple JS to toggle menu and search
     const navToggle = document.getElementById("nav-toggle");
     const navLinks = document.getElementById("nav-links");
@@ -15,22 +14,4 @@
       mobileSearch.classList.toggle("active");
       searchToggle.querySelector("span").textContent =
         mobileSearch.classList.contains("active") ? "close" : "search";
-=======
-// Simple JS to toggle menu and search
-    const navToggle = document.getElementById("nav-toggle");
-    const navLinks = document.getElementById("nav-links");
-    const searchToggle = document.getElementById("search-toggle");
-    const mobileSearch = document.getElementById("mobile-search");
-
-    navToggle.addEventListener("click", () => {
-      navLinks.classList.toggle("active");
-      navToggle.querySelector("span").textContent =
-        navLinks.classList.contains("active") ? "close" : "menu";
-    });
-
-    searchToggle.addEventListener("click", () => {
-      mobileSearch.classList.toggle("active");
-      searchToggle.querySelector("span").textContent =
-        mobileSearch.classList.contains("active") ? "close" : "search";
->>>>>>> 169cdf0033318ba37326fa754a0a1d4c34896f1a
     });
